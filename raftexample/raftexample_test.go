etcd-io/etcd

FSM.ProcessCommits(): return an error rather than calling `log.Fatal()`
Handle such errors at the caller, instead.

Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

contrib/raftexample/raftexample_test.go
@@ -41,7 +41,8 @@ type peer struct {
 
 type nullFSM struct{}
 
-func (nullFSM) ProcessCommits(commitC <-chan *commit, errorC <-chan error) {
+func (nullFSM) ProcessCommits(commitC <-chan *commit, errorC <-chan error) error {
+	return nil
 }
 
 func (nullFSM) TakeSnapshot() ([]byte, error) {
@@ -224,7 +225,11 @@ func TestPutAndGetKeyValue(t *testing.T) {
 		proposeC, confChangeC,
 	)
 
-	go fsm.ProcessCommits(commitC, errorC)
+	go func() {
+		if err := fsm.ProcessCommits(commitC, errorC); err != nil {
+			log.Fatalf("raftexample: %v", err)
+		}
+	}()
 
 	srv := httptest.NewServer(&httpKVAPI{
 		store:       kvs,

etcd-io/raft

raftexample/raftexample_test.go
@@ -41,7 +41,8 @@ type peer struct {
 
 type nullFSM struct{}
 
-func (nullFSM) ProcessCommits(_ <-chan *commit, _ <-chan error) {
+func (nullFSM) ProcessCommits(_ <-chan *commit, _ <-chan error) error {
+	return nil
 }
 
 func (nullFSM) TakeSnapshot() ([]byte, error) {
@@ -226,7 +227,11 @@ func TestPutAndGetKeyValue(t *testing.T) {
 		proposeC, confChangeC,
 	)
 
-	go fsm.ProcessCommits(commitC, errorC)
+	go func() {
+		if err := fsm.ProcessCommits(commitC, errorC); err != nil {
+			log.Fatalf("raftexample: %v", err)
+		}
+	}()
 
 	srv := httptest.NewServer(&httpKVAPI{
 		store:       kvs,