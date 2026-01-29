af1d33d48ed525762f1d839d2f00b4ce2065b052
Khiem Nguyen <khiem.nguyen@solidstatescientific.com>
2026-01-29 17:41:33 +0000

    add POST and DELETE to add and delete nodes
    - POST method doens't work at the moment.

--- contrib/raftsimple/main.go
+++ contrib/raftsimple/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"sync"
+
+	"go.etcd.io/raft/v3/raftpb"
 )
 
 func main() {
@@ -21,16 +23,17 @@ func main() {
 	for i := range *nodesCount {
 		id := uint64(i + 1)
 		proposeC := make(chan string)
+		confChangeC := make(chan raftpb.ConfChange)
 		commitC := make(chan *commit)
 		errorC := make(chan error)
 
-		rn := newRaftNode(id, peers, nw, proposeC, commitC, errorC)
+		rn := newRaftNode(id, peers, nw, proposeC, confChangeC, commitC, errorC)
 		nw.peers[id] = rn
 
 		kvs := newKVStore(proposeC, commitC, errorC)
 
 		wg.Add(1)
-		go serveHTTPKVAPI(kvs, 9121+i, errorC, &wg)
+		go serveHTTPKVAPI(kvs, 9121+i, confChangeC, errorC, &wg)
 	}
 
 	log.Println("Main goroutine waiting for workesr to finish...")
