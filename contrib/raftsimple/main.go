88008fec6d993e218818c17c36fc47161bb2b214
Khiem Nguyen <khiem.nguyen@solidstatescientific.com>
2026-01-30 14:18:46 +0000

    isolate creating raftNodes to a struct
    Signed-off-by: Khiem Nguyen <ppkhiemnguyen@gmail.com>

--- contrib/raftsimple/main.go
+++ contrib/raftsimple/main.go
@@ -8,48 +8,54 @@ import (
 	"go.etcd.io/raft/v3/raftpb"
 )
 
+type orchest struct {
+	raftNodes map[uint64]*raftNode
+	wg        sync.WaitGroup
+}
+
+func (oc *orchest) createNode(nodeID uint64, peers []uint64) {
+	proposeC := make(chan string)
+	confChangeC := make(chan raftpb.ConfChange)
+	commitC := make(chan *commit)
+	errorC := make(chan error)
+	rn := newRaftNode(nodeID, peers, proposeC, confChangeC, commitC, errorC)
+	for nid, node := range oc.raftNodes {
+		rn.nw.addPeer(nid, node)
+	}
+
+	oc.raftNodes[nodeID] = rn
+
+	for _, node := range oc.raftNodes {
+		node.nw.addPeer(nodeID, rn)
+	}
+
+	kvs := newKVStore(proposeC, commitC, errorC)
+
+	oc.wg.Add(1)
+	go func() {
+		defer oc.wg.Done()
+		serveHTTPKVAPI(kvs, 9120+int(nodeID), confChangeC, errorC)
+		log.Printf("node %d: KVstore has stopped running\n", nodeID)
+	}()
+}
+
 func main() {
 	nodesCount := flag.Int("nodes", 3, "number of nodes")
 	flag.Parse()
 
-	var wg sync.WaitGroup
+	oc := orchest{raftNodes: make(map[uint64]*raftNode, *nodesCount)}
 
 	peers := make([]uint64, *nodesCount)
 	for i := range *nodesCount {
 		peers[i] = uint64(i + 1)
 	}
 
-	raftNodes := make(map[uint64]*raftNode, *nodesCount)
-
 	for i := range *nodesCount {
 		id := uint64(i + 1)
-		proposeC := make(chan string)
-		confChangeC := make(chan raftpb.ConfChange)
-		commitC := make(chan *commit)
-		errorC := make(chan error)
-
-		rn := newRaftNode(id, peers, proposeC, confChangeC, commitC, errorC)
-		for nid, node := range raftNodes {
-			rn.nw.addPeer(nid, node)
-		}
-
-		raftNodes[id] = rn
-
-		for _, node := range raftNodes {
-			node.nw.addPeer(id, rn)
-		}
-
-		kvs := newKVStore(proposeC, commitC, errorC)
-
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			serveHTTPKVAPI(kvs, 9121+i, confChangeC, errorC)
-			log.Printf("node %d: KVstore has stopped running\n", id)
-		}()
+		oc.createNode(id, peers)
 	}
 
 	log.Println("Main goroutine waiting for workesr to finish...")
-	wg.Wait()
+	oc.wg.Wait()
 	log.Println("All workers finished, main goroutine exiting.")
 }
