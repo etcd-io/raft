0c971d69c7bcc225f1b57c446e1c78fb586c0563
Khiem Nguyen <ppkhiemnguyen@gmail.com>
2026-03-01 19:23:52 -0500

    add mutex
    Signed-off-by: Khiem Nguyen <ppkhiemnguyen@gmail.com>

--- contrib/raftsimple/network.go
+++ contrib/raftsimple/network.go
@@ -3,15 +3,19 @@ package main
 import (
 	"context"
 	"log"
+	"sync"
 
 	"go.etcd.io/raft/v3/raftpb"
 )
 
 type network struct {
+	mu    sync.RWMutex
 	peers map[uint64]*raftNode
 }
 
 func (nw *network) register(nodeID uint64, rn *raftNode) {
+	nw.mu.Lock()
+	defer nw.mu.Unlock()
 	for _, node := range nw.peers {
 		node.t.addPeer(nodeID)
 	}
@@ -19,19 +23,33 @@ func (nw *network) register(nodeID uint64, rn *raftNode) {
 }
 
 func (nw *network) deregister(nodeID uint64) {
+	nw.mu.Lock()
+	defer nw.mu.Unlock()
 	delete(nw.peers, nodeID)
 }
 
 func (nw *network) send(m raftpb.Message) {
+	nw.mu.RLock()
 	p, ok := nw.peers[m.To]
+	nw.mu.RUnlock()
+
 	if !ok {
 		log.Printf("node %d: unable to find node %d to send %s\n", m.From, m.To, m.Type.String())
 		return
 	}
+
+	if p.node == nil {
+		// Node is registered but not yet initialized. Drop the message.
+		// Raft will retry once the node is up.
+		return
+	}
+
 	_ = p.node.Step(context.TODO(), m)
 }
 
 func (nw *network) getNode(nodeID uint64) (*raftNode, bool) {
+	nw.mu.RLock()
+	defer nw.mu.RUnlock()
 	n, ok := nw.peers[nodeID]
 	return n, ok
 }
