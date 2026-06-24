dac86148718b37f87569c3517f1198a70527b97b
Takuto Nagami <logica0419@gmail.com>
2026-06-24 12:23:44 +0900

    fix the variable name based on the flag name
    Signed-off-by: Takuto Nagami <logica0419@gmail.com>

--- rawnode_test.go
+++ rawnode_test.go
@@ -990,7 +990,7 @@ func BenchmarkRawNode(b *testing.B) {
 func benchmarkRawNodeImpl(b *testing.B, peers ...uint64) {
 	s := newTestMemoryStorage(withPeers(peers...))
 	cfg := newTestConfig(1, 10, 1, s)
-	if !*debug {
+	if !*benchDebug {
 		cfg.Logger = discardLogger // avoid distorting benchmark output
 	}
 	rn, err := NewRawNode(cfg)
@@ -1006,7 +1006,7 @@ func benchmarkRawNodeImpl(b *testing.B, peers ...uint64) {
 		for rn.HasReady() {
 			numReady++
 			rd := rn.Ready()
-			if *debug {
+			if *benchDebug {
 				b.Log(DescribeReady(rd, nil))
 			}
 			if n := len(rd.CommittedEntries); n > 0 {
@@ -1016,7 +1016,7 @@ func benchmarkRawNodeImpl(b *testing.B, peers ...uint64) {
 			for _, m := range rd.Messages {
 				if m.GetType() == pb.MsgVote {
 					resp := &pb.Message{To: m.From, From: m.To, Term: m.Term, Type: pb.MsgVoteResp.Enum()}
-					if *debug {
+					if *benchDebug {
 						b.Log(DescribeMessage(resp, nil))
 					}
 					rn.Step(resp)
@@ -1027,7 +1027,7 @@ func benchmarkRawNodeImpl(b *testing.B, peers ...uint64) {
 						idx = m.GetEntries()[n-1].GetIndex()
 					}
 					resp := &pb.Message{To: m.From, From: m.To, Type: pb.MsgAppResp.Enum(), Term: m.Term, Index: new(idx)}
-					if *debug {
+					if *benchDebug {
 						b.Log(DescribeMessage(resp, nil))
 					}
 					rn.Step(resp)
